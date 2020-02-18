77b9b0738d002caa91f6a440b2c1631ecd6bd1e7
Ludvig Alexander Brüchmann <ludvigbruchmann@gmail.com>
2020-02-18 02:28:07 +0100

    Added status command

--- command.js
+++ command.js
@@ -1,12 +1,17 @@
 const colors = require('colors')
 const debug = require('./debug')
 const config = require('./config')
+const funnies = require('./funnies')
 const md = require('./md')
 
 module.exports = {
   parse: (msg, callback=(cmd,args)=>{}) => {
     if(msg.content.startsWith(config.prefix)){
       msgArray = msg.content.match(/(".*?"|[^"\s]+)+(?=\s*|\s*$)/g)
+      for (let i = 0; i < msgArray.length; i++) {
+        msgArray[i] = msgArray[i].replace(/^"(.+(?="$))"$/, '$1')
+        
+      }
       cmd = msgArray[0].substr(1)
       args = msgArray.slice(1)
       debug.log(`${colors.blue("@" + msg.author.username) + " in " + colors.blue("#" + msg.channel.name)} > ${colors.green(config.prefix + cmd)} > ${args.join(", ")}`)
@@ -27,6 +32,13 @@ module.exports = {
         }
         break
 
+      case "status":
+        if(config.gods.includes(msg.author.id) && args.length >= 2){
+          client.user.setPresence({game: {name: args[1], type: args[0].toUpperCase()}, status: "online"})
+          debug.log("Changed status: " + colors.blue(`${args[0]} ${args[1]}`))
+        }
+        break
+
       case "roll":
         if(args[0]){
           dice = args[0].replace(/\D/g,'')
@@ -37,6 +49,7 @@ module.exports = {
         roll = Math.floor(Math.random() * dice) + 1
 
         msg.channel.send(`:game_die: ${msg.author} rolled ${md.code(roll)} out of ${md.code(dice)}`)
+        debug.log(`${colors.blue("@"+msg.author.username)} rolled ${colors.green(roll)} out of ${colors.green(dice)}`)
         break
     
       default:
